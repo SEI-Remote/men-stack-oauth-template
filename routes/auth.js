916a061eb0a8819bef14339900b45edb2ce63f10
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-09-17 14:28:45 -0500

    fix code formatting

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,10 @@ import passport from 'passport'
 
 const router = Router()
 
-router.post(
-  "/google",
-  passport.authenticate("google-one-tap", {
-    failureRedirect: "/",
-    successRedirect: "/",
-  }),
-);
+router.post("/google", passport.authenticate("google-one-tap", {
+  failureRedirect: "/",
+  successRedirect: "/",
+}))
 
 router.get('/logout', function (req, res, next) {
   req.logout(function(err) {
