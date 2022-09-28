a4033efd962b9c38d3b3b7361d547298fa4871b1
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-09-28 18:15:37 -0500

    cleanup code

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// import npm packages
 import 'dotenv/config.js'
 import express from 'express'
 import path from 'path'
@@ -7,15 +8,17 @@ import session from 'express-session'
 import logger from 'morgan'
 import methodOverride from 'method-override'
 import passport from 'passport'
+
+// import custom middleware
 import { passDataToView } from './middleware/middleware.js'
 
 // connect to MongoDB with mongoose
-import('./config/database.js')
+import './config/database.js'
 
 // load passport
-import('./config/passport.js')
+import'./config/passport.js'
 
-// require routes
+// import routes
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 
@@ -29,7 +32,7 @@ app.set(
 )
 app.set('view engine', 'ejs')
 
-// middleware
+// basic middleware
 app.use(methodOverride('_method'))
 app.use(logger('dev'))
 app.use(express.json())
@@ -55,7 +58,8 @@ app.use(
 // passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
-// new code here
+
+// custom middleware
 app.use(passDataToView)
 
 // router middleware
@@ -76,7 +80,8 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error', {
     title: `🎊 ${err.status || 500} Error`,
-    user: req.user ? req.user : null
+    user: req.user ? req.user : null,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
   })
 })
 
