ae38af8017a1d73a3c83317a5a064bee07478cd2
teradaian <teradaian@gmail.com>
2023-01-12 14:18:15 -0800

    Revert "add index controller"
    This reverts commit cbe71f2f336ef8eb245a6ef5a642f468e7bf346b.

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ import'./config/passport.js'
 // import routes
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
-import { router as tacosRouter } from './routes/tacos.js'
-
 
 // create the express app
 const app = express()
@@ -67,7 +65,6 @@ app.use(passDataToView)
 // router middleware
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
-app.use('/tacos', tacosRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
